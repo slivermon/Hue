46190504692588b535f82680c7db7f128f10fb27
Shawn Livermon <shawn.livermon@gmail.com>
2015-09-28 00:34:05 -0700

    Partially refactored

--- hue_local.py
+++ hue_local.py
@@ -1,25 +1,29 @@
 import requests
 import json
+import yaml
+#import pandas as pd
 # To access debug panel http://<bridge ip address>/debug/clip.html
 # More about the 'requests' module:  http://docs.python-requests.org/en/latest/user/quickstart/#more-complicated-post-requests
 
-# For easy debug, skip data entry and use stored values for api and IP
-skip_entry = False
-
-print('Welcome to the Hue API Experimenter.')
-
-if skip_entry == False:
-    # Manually enter the api key and IP address
-    api_key = input('Please enter the API key:')
-    ip_address = input('Please enter the IP address of the bridge on the local network.')
-else:
-    # Remove this later, not secure to store API key here
-    api_key = ''
-    ip_address = ''
+# Get API key and local IP address
+credentials = yaml.load(open('credentials.yml'))
+api_key = credentials['hue_api_key']
+ip_address = credentials['ip']
 
 # Construct the API request URL
-base_url = 'http://' + str(ip_address) + '/api/' + api_key + '/lights/'
-print(base_url)
+url = "http://{0}/api/{1}/lights".format(ip_address, api_key)
+print(url)
+
+# Get a list of all lights
+resp = requests.get(url)
+data_lights = json.loads(resp.text) # requests library's .text shows JSON
+
+# Create menu objects from JSON
+light_list = {}
+for light in data_lights.keys():
+    light_list.append(data_lights[light]['name'])
+
+
 
 # Light change menu; would be great to pull the list from the API next time
 print('Which light do you want to use?')
@@ -35,7 +39,7 @@ brightness = input('Enter new brightness value (1 - 254): ')
 brightness = int(brightness) # Must be an integer or it will error
 
 # Create URL and payload
-change_brightness_url = base_url + select_light  + '/state/'
+change_brightness_url = url + select_light  + '/state/'
 brightness_payload = {'bri': brightness}
 
 # For debug
@@ -48,5 +52,3 @@ print()
 print('Changing brightness...')
 request = requests.put(change_brightness_url, data=json.dumps(brightness_payload)) # Store response in 'request' variable; data=json.dumps() is for json payloads, params=variable is for query strings
 print(request)
-
-
